9f6ae50651dd4a33a2949b691e3625e675958fd3
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-04-11 05:15:11 -0300

    Update index.ts

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,8 +2,8 @@ import { ChainId } from 'sdkv2'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
-  [ChainId.BSCTESTNET]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A'
+  [ChainId.MAINNET]: '0x8db97c7cece249c2b98bdc0226cc4c2a57bf52fc', // TODO
+  [ChainId.BSCTESTNET]: '0x88Da55CE7185Fd86a2C15FE93b05F84E25cFeEB1'
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
