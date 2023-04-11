c7b6424ef59eac92a5782f71c77c830852cf4430
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-04-11 05:13:28 -0300

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ import { ChainId, JSBI, Percent, Token, WETH } from 'sdkv2'
 import { AbstractConnector } from '@web3-react/abstract-connector'
 
 // import { bc, fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
-import { injected, bsc } from '../connectors'
+import { injected, bc } from '../connectors'
 // TODO
 export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
 
